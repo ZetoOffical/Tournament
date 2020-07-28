c02641c7e33e335b4d21ce1f2f747cf4e5d071cc
GoodMan5263 <68381689+GoodMan5263@users.noreply.github.com>
2020-07-28 21:04:37 +0400

    Add files via upload

--- projects/GoodMan/fakesite.py
+++ projects/GoodMan/fakesite.py
@@ -4,11 +4,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
- 
+
 @app.route('/sort', methods=['POST'])
 def sort():
-resp = {'success': True, 'data': sorted(requests.json()['data'], key=lambda x: int(x['_id']))}
-	return jsonify(resp)
+    resp = {'success': True, 'data': sorted(requests.json()['data'], key=lambda x: int(x['_id']))}
+    return jsonify(resp)
 
 
 if __name__ == '__main__':
