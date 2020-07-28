282454566c96b95f4936c3da2f878fb70ead675a
GoodMan5263 <68381689+GoodMan5263@users.noreply.github.com>
2020-07-28 20:38:38 +0400

    Add files via upload

---
+++ projects/GoodMan/fakesite.py
@@ -0,0 +1,23 @@
+import json
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/sort', methods=['POST'])
+def sort():
+	a = {success: True,
+    data: [{"_id": "80808080", "name": "Anton"}, {"_id": "90909090", "name": "Gosha"}]}
+	resp = {'success': True, 'data': sorted(a['data'], key=lambda x: int(x['_id']))}
+	return jsonify(resp)
+
+@app.route('/search', methods=['POST'])
+def search():
+	pass
+
+@app.route('/parse', methods=['POST'])
+def parse():
+	pass
+
+if __name__ == '__main__':
+	app.run(debug=True)
