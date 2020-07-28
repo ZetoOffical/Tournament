3cb7d29f6b30a9b0072940ed8d1d18d4f44c3025
GoodMan5263 <68381689+GoodMan5263@users.noreply.github.com>
2020-07-28 21:01:10 +0400

    Update fakesite.py

--- projects/GoodMan/fakesite.py
+++ projects/GoodMan/fakesite.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
+ 
 @app.route('/sort', methods=['POST'])
 def sort():
 	resp = {'success': True, 'data': sorted(requests.json()['data'], key=lambda x: int(x['_id']))}
