d6c76b1e8bf516d91136a2f50e962d8b1c7516f2
catarium <60063501+catarium@users.noreply.github.com>
2020-07-28 19:29:23 +0300

    we5r6

---
+++ projects/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+a_lst = [
+
+]
+
+
+@app.route('/sort')
+def sort():
+    b = {
+        'asc': request.json['asc'],
+        'array': request.json['array']
+    }
+    c = b['array']
+    sort(c, key=lambda x: x['name'])
+    print(c)
+    return jsonify({'b': {'succes': True, 'array': c}})
+
+
+app.run(debug=True)
---
+++ projects/main2.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+a_lst = [
+
+]
+
+
+@app.route('/search')
+def sort():
+    if 'ignorecase' not in request.json:
+        b = {
+            'string': request.json['string'],
+            'search': request.json['search'],
+            'match': request.json['match'],
+        }
+
+    else:
+        b = {
+            'string': request.json['string'],
+            'search': request.json['search'],
+            'match': request.json['match'],
+            'ignorecase': request.json['ignorecase']
+        }
+        s1 = b['string']
+        s2 = b['search']
+        s3 = b['match']
+        s4 = b['ignorecase']
+        if s4:
+            s1 = s1.lower()
+        if s3 == 'any':
+            r1 = s1.find(s2)
+            if r1:
+                r2 = r1 + len(s2) - 1
+                return jsonify({'success': True, 'data': [r1, r2]})
+            else:
+                return jsonify({'success': True, 'data': []})
+        else:
+            if s2 in s1.split():
+                r1 = s1.find(s2)
+                r2 = r1 + len(s2) - 1
+                return jsonify({'success': True, 'data': [r1, r2]})
+            else:
+                return jsonify({'success': True, 'data': []})
+
+
+app.run(debug=True)
